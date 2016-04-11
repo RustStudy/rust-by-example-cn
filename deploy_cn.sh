2cfd768931394d4ccdeeea3a0f28d7966d2cc2c2
blackanger <blackanger.z@gmail.com>
2016-04-11 11:18:58 +0800

    modify

--- deploy_cn.sh
+++ deploy_cn.sh
@@ -7,7 +7,7 @@ cd stage/_book
 git init
 git config user.name "blackanger"
 git config user.email "blackanger.z@gmail.com"
-git remote add upstream "https://$GH_TOKEN@github.com/RustStudy/rust-by-example-cn.git"
+git remote add upstream "https://ZhangHanDong@github.com/RustStudy/rust-by-example-cn.git"
 git fetch upstream && git reset upstream/gh-pages
 
 # echo "rustbyexample.com" > CNAME
